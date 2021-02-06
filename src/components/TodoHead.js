60ddd734858d0b62f66756d4d8ce273df00ea3f9
prsn-ts <turbo6040@gmail.com>
2021-02-06 13:15:55 +0900

    useTodoState 함수같은 커스텀 hook을 통해 undonetasks 변수에 상태값을 저장 및 관리/Date 객체를 통해 날짜, 시간을 나타내는 TodoHead.js 파일 수정함.

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -36,12 +36,29 @@ const TodoHead = styled.div`
 
 function Head() {
     const todos = useTodoState(); //useReducer로 상태를 관리하는 context 가져와서 todos 변수에 저장.
-    console.log(todos); //결과 확인해보기
+    const undonetasks = todos.filter(todo => !todo.done); //done 키에 저장된 값중 false인 것만 필터링하여 가져와서 undonetasks 변수에 저장
+    console.log(undonetasks); //결과 확인해보기
+
+    /* 
+        날자와 시간관련 toLocaleDateString 메소드에 관한 내용은 MDN에서 확인가능하다.(아래 주소에서 확인가능)
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    */
+    const today = new Date(); //인수를 전달하지 않으면 현재 날짜와 시간을 가지는 인스턴스를 반환한다.
+    const dateString = today.toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }); //년도, 월, 일 등의 날짜를 문자열로 dateString 변수에 저장한다.
+    const dayName = today.toLocaleDateString('ko-KR', {weekday: 'long'}); //요일에 해당하는 문자열 값을 dayName 변수에 저장한다.
+
+    console.log(dateString); //결과 확인해보기
+    console.log(dayName); //결과 확인해보기
+
     return(
         <TodoHead>
-            <h1>2021년 1월 23일</h1>
-            <div className="day">토요일</div>
-            <div className="tasks_left">할 일 2개 남음</div>
+            <h1>{dateString}</h1>
+            <div className="day">{dayName}</div>
+            <div className="tasks_left">할 일 {undonetasks.length}개 남음</div>
         </TodoHead>
     );
 }
